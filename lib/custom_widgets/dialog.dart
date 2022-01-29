344f8ec664b354b953ccc38b15629c2b3dad1061
Hari Govind K K <45679986+harigovindkk@users.noreply.github.com>
2022-01-29 16:31:24 +0530

    Update dialog.dart

--- lib/custom_widgets/dialog.dart
+++ lib/custom_widgets/dialog.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:map_example/constants/colors.dart';
+import 'package:map_example/custom_widgets/earnings_card.dart';
 import 'package:map_example/custom_widgets/previous_delivery_card.dart';
 import '../constants/resources.dart';
-
 Dialog profileDetails = Dialog(
   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
   child: ListView(
+    physics:const NeverScrollableScrollPhysics(),
     shrinkWrap: true,
     children: <Widget>[
       const SizedBox(height: 20),
@@ -41,100 +41,44 @@ Dialog profileDetails = Dialog(
         height: 2,
       ),
       Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-            child: Column(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      height: 60,
-                      width: 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          border: Border.all(color: primary, width: 5)),
-                      child: Center(
-                          child: Text(
-                        '₹100',
-                        style: GoogleFonts.poppins(),
-                      )),
-                    ),
-                    Text(
-                      'Total Earnings',
-                      style: GoogleFonts.poppins(),
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                border: Border.all(color: primary, width: 5)),
-                            child: Center(
-                                child: Text(
-                              '₹40',
-                              style: GoogleFonts.poppins(),
-                            ))),
-                        Text(
-                          'Todays Earnings',
-                          style: GoogleFonts.poppins(),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                border: Border.all(color: primary, width: 5)),
-                            child: Center(
-                                child: Text(
-                              '₹60',
-                              style: GoogleFonts.poppins(),
-                            ))),
-                        Text(
-                          'Average Earnings',
-                          style: GoogleFonts.poppins(),
-                        )
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: primary, width: 1))),
+        padding: const EdgeInsets.only(top: 20.0, left: 20),
+        child: Text(
+          'My Earnings',
+          style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: const [
+            EarningsCard(50, Icons.today, 'Today'),
+            EarningsCard(60, Icons.bar_chart,'Average'),
+            EarningsCard(100, Icons.account_balance_wallet, 'Total'),
+          ],
+        ),
       ),
       const Divider(
         color: Colors.grey,
         height: 2,
       ),
       Padding(
-        padding: const EdgeInsets.only(top: 10.0, left: 20),
+        padding: const EdgeInsets.only(top: 20.0, left: 20),
         child: Text(
           'Previous Orders',
           style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
         ),
       ),
-      Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: const [
-            HistoryCard('Sagar Restaurant', '27/01/2022',
-                'https://assets.quickerala.com/photo/photo_320213_607d5c905293d.jpg'),
-            HistoryCard('Paragon Restaurant', '28/01/2021',
-                'https://upload.wikimedia.org/wikipedia/commons/4/46/Paragonrestlogo.jpg')
-          ],
+      SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: const [
+              HistoryCard('Sagar Restaurant', '27/01/2022',
+                  'https://assets.quickerala.com/photo/photo_320213_607d5c905293d.jpg'),
+              HistoryCard('Paragon Restaurant', '28/01/2021',
+                  'https://upload.wikimedia.org/wikipedia/commons/4/46/Paragonrestlogo.jpg'),
+            ],
+          ),
         ),
       ),
       const SizedBox(height: 20),
